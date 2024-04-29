934830325beef8bf87d604d06c35d343a854ce79
SaraFerrari12091999 <165146888+SaraFerrari12091999@users.noreply.github.com>
2024-04-29 10:48:48 +0200

    Update index.js

--- nisolog-app/src/index.js
+++ nisolog-app/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Server in ascolto sulla porta ${port}`);
