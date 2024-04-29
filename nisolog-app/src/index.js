4b0aa53169cee13620dc75bceb349e95f07450a5
SaraFerrari12091999 <165146888+SaraFerrari12091999@users.noreply.github.com>
2024-04-29 10:09:38 +0200

    Update index.js

--- nisolog-app/src/index.js
+++ nisolog-app/src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server in ascolto sulla porta ${port}`);
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
